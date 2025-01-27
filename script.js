6eb161b23444cb290fc54b2c1b93c197a3d01242
JL22-school <lealjack229@gmail.com>
2025-01-26 22:21:52 -0600

    button

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ var statusLabel = document.getElementById('statusLabel');
 statusLabel.textContent = "Loaded";
 });
 
-const clickButton = document.getElementById('clickButton');
-const textInput = document.getElementById('textInput');
-const myList = document.getElementById('myList');
-
-addButton.addEventListener('click', function() {
-    const newItemText = textInput.value;
-    if (newItemText.trim() !== '') {
-        const newListItem = document.createElement('li'); 
-        newListItem.textContent = newItemText;
-        myList.appendList(newListItem);
-        textInput.value = '';
-    }
+window.addEventListener('DOMContentLoaded', function() {
+    const clickButton = document.getElementById('clickButton');
+    const textInput = document.getElementById('textInput');
+    const myList = document.getElementById('myList');
+    
+    clickButton.addEventListener('click', function() {
+        const newItemText = textInput.value;
+        if (newItemText.trim() !== '') {
+            const newListItem = document.createElement('li'); 
+            newListItem.textContent = newItemText;
+            myList.appendList(newListItem);
+            textInput.value = '';
+        });
 });
