eb1f4b37da8553b14f32c503944bcd620eeca133
JL22-school <lealjack229@gmail.com>
2025-01-26 22:09:14 -0600

    addbutton

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const clickButton = document.getElementById('clickButton');
 const textInput = document.getElementById('textInput');
 const myList = document.getElementById('myList');
+
+addButton.addEventListener('click', function() {
+    const newItemText = textInput.value;
+    if (newItemText.trim() !== '') {
+        const newListItem = document.createElement('li'); 
+        newListItem.textContent = newItemText;
+        myList.appendList(newListItem);
+        textInput.value = '';
+    }
+});
